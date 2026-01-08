b9e7acf4a2a2a1b60f6ffec8912870e0fb68008f
manishhelpoffcial-svg <manish.help.offcial@gmail.com>
2026-01-08 14:53:54 +0000

    Fixed Favicon error

--- app.py
+++ app.py
@@ -244,5 +244,5 @@ def static_files(path):
     return send_from_directory(DIRECTORY, path)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
